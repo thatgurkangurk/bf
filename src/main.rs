7ebb4cef3e5c5a271f1ff6a6960c42ea29379a1c
Gurkan <hello@gurkz.me>
2023-09-19 16:03:59 +0200

    refactor and change the cli to clap

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,24 @@
-use std::{env, fs::File, io::Read};
-
+use args::parse_args;
+use file::read_file_contents;
 use lexer::lex;
 use parser::parse;
 use runner::run;
 
+mod args;
+mod file;
 mod instructions;
 mod lexer;
 mod parser;
 mod runner;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() != 2 {
-        println!("usage: yabfr <any_file.bf>");
-        std::process::exit(1);
-    }
-
-    let filename = &args[1];
-
-    // read the file
-    let mut file = File::open(filename).expect("that file does not exist");
-    let mut source = String::new();
-    file.read_to_string(&mut source)
-        .expect("failed to read the file");
-
+    let args = parse_args();
+    let file = read_file_contents(&args.file);
     // lex the file
-    let operation_codes = lex(source);
+    let operation_codes = lex(file);
 
     // parse opcodes
     let program = parse(operation_codes);
 
-    // set up env and run
-    let mut tape: Vec<u8> = vec![0; 1024];
-    let mut data_ptr = 512;
-
-    run(&program, &mut tape, &mut data_ptr)
+    run(&program);
 }
